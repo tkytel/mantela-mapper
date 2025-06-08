4f726b0c170b645a611885325b76613284315e76
ねじくぎ/nejikugi <63852665+scrwnl@users.noreply.github.com>
2025-06-08 12:36:27 +0900

    Add error report (#7)

--- main.js
+++ main.js
@@ -10,9 +10,14 @@ L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
 document.getElementById("formMantela").addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    // エラー欄を全消去します
+    const cloneOutputError = outputError.cloneNode(false);
+    outputError.parentNode.replaceChild(cloneOutputError, outputError);
+    summaryError.textContent = "エラー情報"
+
+    outputStatus.textContent = "";
     btnGenerate.disabled = true;
     const start = performance.now();
-    outputStatus.textContent = "";
     const request = checkNest.checked
 		? await fetchMantelas3(urlMantela.value, {maxDepth: +numNest.value})
 		: await fetchMantelas3(urlMantela.value)
@@ -47,7 +52,6 @@ document.getElementById("formMantela").addEventListener("submit", async (e) => {
     });
     extensions.filter((extension)=>{
       if (extension.geolocationCoordinates != undefined) {
-        console.log(extension)
         return true;
       }
       return false;
@@ -59,6 +63,34 @@ document.getElementById("formMantela").addEventListener("submit", async (e) => {
         +`緯度: ${extension.geolocationCoordinates.latitude}<br>`
         +`経度: ${extension.geolocationCoordinates.longitude}`, {autoclose: false});
     })
+
+    summaryError.textContent = `エラー情報（${request.errors.length}件）`
+    request.errors.forEach((error) => {
+      const dt = document.createElement("dt");
+      dt.textContent = error.message;
+
+      const ddNameMesg = document.createElement("dd");
+      ddNameMesg.textContent = {
+        TypeError:
+          "Mantela.json の取得に失敗した可能性があります"
+          +"（CORS の設定や HTTP ヘッダを確認してみてください）",
+        Error:
+          "Mantela.json の取得に失敗した可能性があります"
+          +"（正しい URL であるか確認してみてください）",
+        SyntaxError:
+          "Mantela.json の取得に失敗した可能性があります"
+          +"（書式に問題がないか確認してみてください）",
+        AbortError:
+          "Mantela.json の取得に失敗した可能性があります"
+          +"（ネットワークの状態を確認してみてください）"
+      }[error.cause.name];
+
+      const ddCause = document.createElement("dd");
+		  ddCause.textContent = String(error.cause);
+
+      cloneOutputError.append(dt, ddNameMesg, ddCause);
+    })
+
 });
 
 /*
